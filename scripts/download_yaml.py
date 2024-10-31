ace3f95b0fb4c92345cdf9cd9e17d7b8c26b0b91
jkim810 <jkim0810@gmail.com>
2024-10-31 16:17:47 -0400

    update

--- scripts/download_yaml.py
+++ scripts/download_yaml.py
@@ -1,12 +1,13 @@
 from tqdm import tqdm
 import requests
 import os
-def download(url = 'https://wcm.box.com/shared/static/mdntp2xf9tjobxeidkw93mg8jysb7nh9.yml', path = 'metadata/gg_config.yml'):
+def download(url = 'https://wcm.box.com/shared/static/mdntp2xf9tjobxeidkw93mg8jysb7nh9.yml', path = 'metadata/ggo_config.yml'):
     os.makedirs('metadata/', exist_ok = True)
     response = requests.get(url, stream=True)
     
     with open(path, "wb") as handle:
         for data in tqdm(response.iter_content()):
             handle.write(data)
+        handle.close()
 
 download()
