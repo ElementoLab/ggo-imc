e686c8fd9a7515d0ca609395ad52899d4d29eda3
jkim810 <jkim0810@gmail.com>
2024-10-31 15:06:57 -0400

    updated figure 2 and 3

--- scripts/celltype_heatmap_info.py
+++ scripts/celltype_heatmap_info.py
@@ -1,34 +1,19 @@
 import imc_analysis as imc
 import os
-from pathlib import Path
-from tqdm import tqdm
 import scanpy as sc
-import pandas as pd
 import matplotlib.pyplot as plt
 
-import anndata
-# import warnings
-# warnings.simplefilter("ignore", UserWarning)
-
 import matplotlib
-sc.settings.set_figure_params(dpi=200, dpi_save=300, fontsize=20)
-matplotlib.rcParams["pdf.fonttype"] = 42
-matplotlib.rcParams["ps.fonttype"] = 42
-matplotlib.rcParams["axes.grid"] = False
 matplotlib.use('Agg')
 
 metadata = imc.utils.parse_yaml('metadata/ggo_config.yml')
 
-
 adata_dict = dict()
 for p in ['PANEL_G', 'PANEL_H']:
     print(f"Reading {metadata[p]['AnnData']['phenotyped_umap_name']}...")
-    adata_dict[p] = sc.read(metadata[p]['AnnData']['phenotyped_umap_name'])
-    adata_dict[p].obs['PID'] = adata_dict[p].obs['description'].str[:6]
-    adata_dict[p].obs['radio'] = pd.Categorical(adata_dict[p].obs['radio'], categories = ['N', 'PNS', 'PS', 'S'], ordered=True)
-    adata_dict[p].obs['pathology'] = pd.Categorical(adata_dict[p].obs['pathology'], categories = ['Normal', 'AIS', 'MIA', 'IAC'], ordered = True)
-
-celltypes = ['celltype_broad']
+    adata_dict[p] = sc.read(
+        metadata[p]['AnnData']['phenotyped_umap_name'],
+        backup_url = metadata[p]['AnnData']['backup_url'])
 
 # for celltype in celltypes:
 #     adata_dict[p] = adata_dict[p][adata_dict[p].obs[celltype] != 'Low Expr.']
@@ -37,7 +22,7 @@ print('Plot celltype heatmap')
 for p in adata_dict:
     imc.pl.celltype_heatmap(
         adata_dict[p],
-        cluster_ids = celltypes,
+        cluster_ids = ['celltype_broad'],
         var_names = metadata[p]['var_celltype_groups'],
         out_dir = f'figures/{p}/celltype/'
     )
@@ -51,11 +36,13 @@ for p in adata_dict:
         n_obs = 100000,
         random_state = 0,
     )
-    for celltype in metadata['CELLTYPES']:
-        fig, ax = plt.subplots(1,1,figsize = (6,5), dpi = 300)
-        sc.pl.umap(adata, color = celltype, frameon = False, show = False)
+    
+    celltype = 'celltype_broad'
 
-        os.makedirs(f'figures/{p}/{celltype}/', exist_ok = True)
-        plt.tight_layout()
-        plt.savefig(f'figures/{p}/{celltype}/umap.pdf')
-        plt.close()
+    fig, ax = plt.subplots(1,1,figsize = (6,5), dpi = 300)
+    sc.pl.umap(adata, color = celltype, frameon = False, show = False)
+
+    os.makedirs(f'figures/{p}/{celltype}/', exist_ok = True)
+    plt.tight_layout()
+    plt.savefig(f'figures/{p}/{celltype}/umap.pdf')
+    plt.close()
--- scripts/epithelial_characterization.py
+++ scripts/epithelial_characterization.py
@@ -1,104 +1,87 @@
-
-import os
-from glob import glob
-from pathlib import Path
-import tifffile
-from tqdm import tqdm
-
 import pandas as pd
 import numpy as np
 
 import scanpy as sc
-import anndata
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import anndata
-import warnings
-warnings.simplefilter("ignore", UserWarning)
-
 import imc_analysis as imc
-from scripts.load_yaml import load
 
-import matplotlib
-sc.settings.set_figure_params(dpi=200, dpi_save=300, fontsize=12)
-matplotlib.rcParams["pdf.fonttype"] = 42
-matplotlib.rcParams["ps.fonttype"] = 42
-matplotlib.rcParams["axes.grid"] = False
-matplotlib.use('Agg')
+metadata = imc.utils.parse_yaml('metadata/ggo_config.yml')
 
-metadata = load('metadata/ggo_config.yml')
+print(f"Reading {metadata['PANEL_H']['AnnData']['phenotyped_umap_name']}...")
+adata = sc.read(
+    metadata['PANEL_H']['AnnData']['phenotyped_umap_name'],
+    backup_url = metadata['PANEL_H']['AnnData']['backup_url'])
 
-p = 'PANEL_H'
-print(f"Reading {metadata[p]['AnnData']['phenotyped_umap_name']}...")
-adata = sc.read(metadata[p]['AnnData']['phenotyped_umap_name'])
-adata.obs['PID'] = adata.obs['description'].str[:6]
-adata.obs['radio'] = pd.Categorical(adata.obs['radio'], categories = ['N', 'PNS', 'PS', 'S'], ordered=True)
-# adata.obs['pathology'] = pd.Categorical(adata.obs['pathology'], categories = ['Normal', 'AIS', 'MIA', 'IAC'], ordered = True)
-adata.obs['pathology'] = adata.obs['pathology'].astype(str).str.replace('Normal', 'N')
-adata.obs['pathology'] = pd.Categorical(adata.obs['pathology'], categories = ['N', 'AIS', 'MIA', 'IAC'], ordered = True)
-
-#epithelial_celltypes = ['Tumor-like (RAGE+)', 'Tumor-like (SFTPC+)', 'Tumor-like', 'Epi.-like (RAGE+)', 'Epi.-like (SFTPC+)']
 celltype_map = {
-	'Tumor-like (RAGE+)':'RAGE+',
-	'Tumor-like (SFTPC+)':'SFTPC+',
-	'Tumor-like':'PanCK+',
-	'Epi.-like (RAGE+)':'RAGE+',
-	'Epi.-like (SFTPC+)':'SFTPC+',
+    'Tumor-like (RAGE+)':'RAGE+',
+    'Tumor-like (SFTPC+)':'SFTPC+',
+    'Tumor-like':'PanCK+',
+    'Epi.-like (RAGE+)':'RAGE+',
+    'Epi.-like (SFTPC+)':'SFTPC+',
 }
+
+# Cell Proportion Figure
+
 adata = adata[adata.obs['celltype'].isin(celltype_map.keys())]
 adata.obs['phenotype'] = adata.obs['celltype'].replace(celltype_map)
 adata.obs['phenotype'] = pd.Categorical(adata.obs['phenotype'], categories = ['PanCK+', 'RAGE+', 'SFTPC+'], ordered = True)
-path_phenotype = adata.obs.groupby(['phenotype', 'pathology']).count()['sample'].unstack()
-path_phenotype = path_phenotype / np.array(path_phenotype.sum(axis = 0))[None,:]
+for cond in ['pathology', 'radio']:
+    phenotype = adata.obs.groupby(['phenotype', cond]).count()['sample'].unstack()
+    phenotype = phenotype / np.array(phenotype.sum(axis = 0))[None,:]
 
-fig, ax = plt.subplots(1,1,figsize = (3,2), dpi = 300)
-path_phenotype.T.reset_index().plot(
-	x='pathology',
-	kind='bar',
-	stacked=True,
-	title='',
-	ax = ax)
+    fig, ax = plt.subplots(1,1,figsize = (3,2), dpi = 300)
+    phenotype.T.reset_index().plot(
+        x=cond,
+        kind='bar',
+        stacked=True,
+        title='',
+        ax = ax)
 
-df_rel = path_phenotype.T
-df_pos = df_rel.cumsum(1)-df_rel/2
+    df_rel = phenotype.T
+    df_pos = df_rel.cumsum(1)-df_rel/2
 
-for i, x in enumerate(df_rel.index):
-	for y in df_rel.columns:
-		# print(x, df_pos.loc[x,y], str(np.round(df_rel.loc[x,y] *100, 1)))
-		plt.text(i, df_pos.loc[x,y], str(np.round(df_rel.loc[x,y] *100, 1)) + '%', va = 'center', ha = 'center')
-sns.despine()
-ax.legend().remove()
-ax.set_xlabel('')
-ax.set_xticklabels(ax.get_xticklabels(), rotation = 0)
+    for i, x in enumerate(df_rel.index):
+        for y in df_rel.columns:
+            # print(x, df_pos.loc[x,y], str(np.round(df_rel.loc[x,y] *100, 1)))
+            plt.text(i, df_pos.loc[x,y], str(np.round(df_rel.loc[x,y] *100, 1)) + '%', va = 'center', ha = 'center')
+    sns.despine()
+    ax.legend().remove()
+    ax.set_xlabel('')
+    ax.set_xticklabels(ax.get_xticklabels(), rotation = 0)
+    plt.tight_layout()
+    plt.savefig(f'figures/epithelial_proportion_{cond}.pdf')
+    plt.close()
+
+
+# EMT Proportion Figure
+
+print(f"Reading {metadata['PANEL_G']['AnnData']['phenotyped_umap_name']}...")
+pg = sc.read(
+    metadata['PANEL_G']['AnnData']['phenotyped_umap_name'],
+    backup_url = metadata['PANEL_G']['AnnData']['backup_url'])
+
+from tqdm import tqdm
+pg_epi = pg[pg.obs['celltype_broad'].isin(['Epithelial-like', 'Epithelial-like (Ki67+)','Mesenchymal-like'])]
+#fig, axes = plt.subplots(2,5, dpi = 300, figsize = (10,4))
+fig, axes = plt.subplots(2,4, dpi = 300, figsize = (8,4))
+
+for i, ax in tqdm(enumerate(axes.flatten())):
+    if int(i/4) == 0:
+        feature = 'radio' #'Radiology'
+    else:
+        feature = 'pathology' #'pred'
+    rad = pg_epi.obs[feature].cat.categories[i % 4]
+    pg_tmp = pg_epi[pg_epi.obs[feature] == rad].copy()
+    sns.kdeplot(pg_tmp.to_df(),
+    x = 'PanCK', y = 'Vimentin', fill = True, ax = ax, cmap = 'YlOrRd')
+    ax.set_title(rad)
+
 plt.tight_layout()
-plt.savefig('figures/epithelial_proportion_pathology.pdf')
+plt.savefig(f'figures/EMT proportion.pdf', bbox_inches = 'tight')
 plt.close()
 
-radio_phenotype = adata.obs.groupby(['phenotype', 'radio']).count()['sample'].unstack()
-radio_phenotype = radio_phenotype / np.array(radio_phenotype.sum(axis = 0))[None,:]
 
-fig, ax = plt.subplots(1,1,figsize = (3,2), dpi = 300)
-radio_phenotype.T.reset_index().plot(
-	x='radio',
-	kind='bar',
-	stacked=True,
-	title='',
-	ax = ax)
-
-df_rel = radio_phenotype.T
-df_pos = df_rel.cumsum(1)-df_rel/2
-
-for i, x in enumerate(df_rel.index):
-	for y in df_rel.columns:
-		# print(x, df_pos.loc[x,y], str(np.round(df_rel.loc[x,y] *100, 1)))
-		plt.text(i, df_pos.loc[x,y], str(np.round(df_rel.loc[x,y] *100, 1)) + '%', va = 'center', ha = 'center')
-sns.despine()
-ax.legend().remove()
-ax.set_xticklabels(ax.get_xticklabels(), rotation = 0)
-ax.set_xlabel('')
-plt.tight_layout()
-plt.savefig('figures/epithelial_proportion_radiology.pdf')
-plt.close()
 
